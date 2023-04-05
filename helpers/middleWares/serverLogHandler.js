597841546397ff7a4b048cf8884aeca263ddabd0
Makiiev Oleksandr <alsermak@gmail.com>
2023-04-05 21:46:31 +0300

    remove comments

---
+++ helpers/middleWares/serverLogHandler.js
@@ -0,0 +1,12 @@
+const fs = require('fs/promises');
+const moment = require('moment');
+
+
+const serverLogHandler = (async (req, res, next) => {
+    const { method, url } = req;
+    const date = moment().format('DD-MM-YYYY_hh:mm:ss');
+    await fs.appendFile('./public/server.log', `\n${method} ${url} ${date}`);
+    next();
+  });
+
+  module.exports = serverLogHandler
