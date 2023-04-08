27c77f301cdb575aa25e08029d2947a8d602fbe0
Makiiev Oleksandr <alsermak@gmail.com>
2023-04-08 13:32:39 +0300

    add config.js and process.env

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
 const app = require('./app');
-// FkYm1pLSRdZL19bI
-const DB_HOST = 'mongodb+srv://Oleksandr:FkYm1pLSRdZL19bI@cluster0.zni0pjr.mongodb.net/contacts_app?retryWrites=true&w=majority'
+const DB_HOST = process.env
 
 mongoose.connect(DB_HOST)
 .then(()=> app.listen(3000))
 .catch(error => console.log(error.message));
 
-// app.listen(3000, () => {
-//   console.log('Server running. Use our API on port: 3000');
-// });
+
