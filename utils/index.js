843660d045a27f8bd5ce65b578579f0bfc55a127
Makiiev Oleksandr <alsermak@gmail.com>
2023-04-11 22:12:03 +0300

    remove useless files

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,7 @@
 const ctrlsWrapper = require('./ctrlsWrapper')
-const regexPatterns = require('./regexPatterns')
 const handleMongooseError = require('./handleMongooseError')
 
 module.exports = {
     ctrlsWrapper,
-   regexPatterns,
    handleMongooseError
 }
