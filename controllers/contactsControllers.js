a94abb5ab1c8a4fc8a42260356cc2e758b637851
Makiiev Oleksandr <alsermak@gmail.com>
2023-04-18 18:28:04 +0300

    fix error handlers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -38,7 +38,7 @@ const updateContact = async (req, res) => {
   const { id } = req.params;
   const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
-    throw new HttpError(404, 'ID not found');
+    throw new HttpError(404, 'Contact not found');
   }
   res.json(result);
 };
--- helpers/middleWares/errorHandler.js
+++ helpers/middleWares/errorHandler.js
@@ -1,16 +1,8 @@
 const errorHandler = (err, req, res, next) => {
   console.log('err.message:', err.message);
-  const { name, code } = err;
-  res
-    .status(
-      (err.statusCode =
-        name === 'MongoServerError' && code === 11000
-          ? 409
-          : err.statusCode || 400) || 500
-    )
-    .json({
-      message: err.message || 'Something went wrong! please, try again later',
-    });
+  res.status(err.statusCode || 500).json({
+    message: err.message || 'Something went wrong! please, try again later',
+  });
   next();
 };
 
